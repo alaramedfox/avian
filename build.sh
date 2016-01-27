b2beaabad0fd72a003c9afc9e4c54321542384fa
BitOfAZeldaFan <bryan.geek64@gmail.com>
2016-01-27 14:45:50 -0600

    Directory overhaul part 1

--- build.sh
+++ build.sh
@@ -1,22 +1,16 @@
 # Assembles everything
 
-echo 'Assembling...' &&
-nasm -f elf boot.asm -o obj/boot.o &&
+echo 'Custom builder v1.1' &&
 
-echo 'Compiling the C kernel...' &&
-gcc -m32 -c kernel.c -o obj/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra &&
+echo 'Assembling...' &&
+nasm -f elf src/boot.asm -o obj/boot.o &&
+
+echo 'Compiling...' &&
+gcc -m32 -c src/kernel.c -o obj/kernel.o -std=gnu99 -ffreestanding -O2 -Wall &&
 
 echo 'Linking...' &&
-#gcc -m32 -T linker.ld -o bin/kernel.bin -ffreestanding -O2 -nostdlib obj/boot.o obj/kernel.o -lgcc
-ld -m elf_i386 -T linker.ld -o bin/kernel.bin obj/boot.o obj/kernel.o &&
+ld -m elf_i386 -T linker.ld -o bin/kernel obj/boot.o obj/kernel.o &&
 
-echo 'Creating bootable filesystem...' &&
-cp bin/kernel.bin CD_root/kernel.bin &&
-#echo 'Creating bootable ISO...'
-#mkisofs -o output.iso \
-#   -b isolinux/isolinux.bin -c isolinux/boot.cat \
-#   -no-emul-boot -boot-load-size 4 -boot-info-table \
-#   CD_root
-
+echo 'Running kernel emulator...' &&
 qemu-system-i386 -kernel bin/kernel.bin -no-kvm &&
 echo 'Finished.'
