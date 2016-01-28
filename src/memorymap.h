539db0df9ae46e72e9f975ea5344296517de21cb
BitOfAZeldaFan <bryan.geek64@gmail.com>
2016-01-28 14:08:34 -0600

    Memory map structure

---
+++ src/memorymap.h
@@ -0,0 +1,55 @@
+/*
+ *		Popcorn Kernel
+ *			File:		memorymap.h
+ *			Purpose:	Maps and manages the core internal memory
+ */
+ 
+#define VRAM_C	80*2							//Define the number of char columns
+#define VRAM_R	24*2							//Define the number of char rows
+#define VGA_C	80								//Width of VGA in bytes
+#define VGA_R	24								//Height of VGA in bytes
+
+#define VRAM_START	0xb8000				//Actual address of system VRAM
+#define VRAM_LENGTH	80*24*2				//Length of VRAM in bytes
+#define VRAM_CHARS	VRAM_LENGTH/2		//Length of VRAM in words
+
+#define CHAR_NEWLINE 0xA					//Define the newline char '\n'
+#define CHAR_TAB	 0x9						//Define the tab char '\t'
+
+#define THIS_COL	MEMORY.INDEX.stdout%(VGA_C)			//Current column in VRAM
+#define THIS_ROW	MEMORY.INDEX.stdout/(VGA_C)			//Current row in VRAM
+
+struct __STREAM {
+	char*		stdout; 						//Access to system video RAM
+	char 		stdin[256];					//The input stream buffer
+};
+
+struct __INDEX {
+	int16_t	stdout;
+	size_t	stdin;
+};
+
+struct __GLOBAL {
+	int8_t	color;
+};
+
+struct __FLAGS {
+	bool 		stdin;
+	bool 		stdout;
+};
+
+struct __MEMORY {
+	struct __STREAM	STDIO;
+	struct __INDEX		INDEX;
+	struct __FLAGS		FLAGS;
+	struct __GLOBAL	GLOBAL;
+};
+
+struct __MEMORY MEMORY;
+
+void memorymap_init()
+{
+	MEMORY.STDIO.stdout = (char*)0xb8000;
+	MEMORY.GLOBAL.color = 0x07;
+	MEMORY.INDEX.stdout = 0;
+}
--- src/stdio.h
+++ src/stdio.h
@@ -13,38 +13,13 @@
 
 #include "colordef.h"						//Color #DEFINEs and functions
  
-#define VRAM_C	80*2							//Define the number of char columns
-#define VRAM_R	24*2							//Define the number of char rows
-#define VGA_C	80								//Width of VGA in bytes
-#define VGA_R	24								//Height of VGA in bytes
-
-#define VRAM_START	0xb8000				//Actual address of system VRAM
-#define VRAM_LENGTH	80*24*2				//Length of VRAM in bytes
-#define VRAM_CHARS	VRAM_LENGTH/2		//Length of VRAM in words
-
-#define CHAR_NEWLINE 0xA					//Define the newline char '\n'
-#define CHAR_TAB	 0x9						//Define the tab char '\t'
-
-#define THIS_COL	vptr%(VGA_C)			//Current column in VRAM
-#define THIS_ROW	vptr/(VGA_C)			//Current row in VRAM
 
 
-extern const char HLINE1, HLINE2, HLINE3;
-extern const char VLINE1, VLINE2;
-extern const char BLOCK1, BLOCK2, BLOCK3, BLOCK4;
+extern const char HLINE1, HLINE2, HLINE3,
+						VLINE1, VLINE2,
+						BLOCK1, BLOCK2, BLOCK3, BLOCK4;
 
-extern void i_listen();						//Declare the listener in the ASM file
-extern void i_ignore();						//Declare the un-listener in ASM
-
-char 	*vram = (char*)VRAM_START;			//Access to system video RAM
-int16_t 	vptr = 0;							//pointer to current vram spot
-color_t 	this_color = 0x07;				//Register for current color
-bool	 	STDIN_FLAG=false;					//0 = no update, 1 = update
-char 		std_stream[256];					//The input stream buffer
-size_t	stdin_size=0;						//Size of buffer
-bool 		VUPDATE=false;						//Flag to update something
-
-void vmove(int16_t);							//Safely move the vram pointer.
+void vmove(int16_t);							//Safely move the MEMORY.STDIO.stdout pointer.
 void print(const char str[]);				//Prints a string starting at vpt
 void clear(void);								//Clears the screen
 void move(int,int);							//Moves to 2D location in VRAM
@@ -52,8 +27,8 @@ void newline(void);							//Increment to start of next row
 void setcolor_pair(color_t,color_t);	//Change the screen color: pairs
 void setcolor(color_t);						//Change the screen color: palette
 void scroll(void);							//Scrolling screen support
-void vram_char(char);						//Safely write to vram[vptr]
-void vram_color(color_t);					//Safely write to vram[vptr+1]
+void vram_char(char);						//Safely write to MEMORY.STDIO.stdout[MEMORY.INDEX.stdout]
+void vram_color(color_t);					//Safely write to MEMORY.STDIO.stdout[MEMORY.INDEX.stdout+1]
 	
 void stdin_clear();							//Flush the input buffer
 void stdin_push(char);						//Push character to stdin
@@ -62,11 +37,11 @@ char stdin_peek();							//Return but do not remove last char
 
 void clear(void) {
 	for(int i=0; i<VRAM_CHARS; i++) {
-		vptr = i;
+		MEMORY.INDEX.stdout = i;
 		vram_char(' ');
 		vram_color(palette(C_WHITE,C_BLACK));
 	}
-	vptr = 0;
+	MEMORY.INDEX.stdout = 0;
 }
 
 inline void newline(void) {
@@ -74,56 +49,59 @@ inline void newline(void) {
 }
 
 inline void move(int row, int col) {
-	vptr = (col + VGA_C * row)-1;
-	if(vptr >= VRAM_CHARS) { scroll(); }
+	MEMORY.INDEX.stdout = (col + VGA_C * row)-1;
+	if(MEMORY.INDEX.stdout >= VRAM_CHARS) { scroll(); }
 }
 
 inline void vmove(int16_t v) {
 	if(v >= VRAM_CHARS) { scroll(); vmove(v-VGA_C); }
-	else { vptr = v; }
+	else { MEMORY.INDEX.stdout = v; }
 }
 
 inline void setcolor_pair(color_t fg, color_t bg) {
-	this_color = palette(fg,bg);
+	MEMORY.GLOBAL.color = palette(fg,bg);
 }
 inline void setcolor(color_t color) {
-	this_color = color;
+	MEMORY.GLOBAL.color = color;
 }
 
-/* Safely write to vram without segfault */
+/* Safely write to MEMORY.STDIO.stdout without segfault */
 void vram_char(char value) {
-	if(vptr >= VRAM_CHARS) { scroll(); vram_char(value); }
-	else { vram[vptr*2] = value; }
+	if(MEMORY.INDEX.stdout >= VRAM_CHARS) { scroll(); vram_char(value); }
+	else { MEMORY.STDIO.stdout[MEMORY.INDEX.stdout*2] = value; }
 }
 
 void vram_color(color_t value) {
-	if(vptr >= VRAM_CHARS) { scroll(); vram_color(value); }
-	else { vram[vptr*2+1] = value; }
+	if(MEMORY.INDEX.stdout >= VRAM_CHARS) { scroll(); vram_color(value); }
+	else { MEMORY.STDIO.stdout[MEMORY.INDEX.stdout*2+1] = value; }
 }
 
 void scroll(void) {
 	/* Shift every row up one row */
 	for(int i=0; i<=VRAM_LENGTH-VRAM_C; i++) {
-		vram[i] = vram[i+VRAM_C];
+		MEMORY.STDIO.stdout[i] = MEMORY.STDIO.stdout[i+VRAM_C];
 	}
 	
 	move(23,0);
 	for(int i=0;i<80; i++) {
-		vptr++;
+		MEMORY.INDEX.stdout++;
 		vram_char(' ');
-		vram_color(this_color);
+		vram_color(MEMORY.GLOBAL.color);
 	}
 	/* Set cursor to start of line */
 	move(23,1);
 }
 
 void print(const char str[]) {
-	/* Write the string to VRAM starting at vptr */
+	/* Write the string to VRAM starting at MEMORY.INDEX.stdout */
 	for(int i=0; str[i] != '\0'; i++) {
-		vptr++;
+		MEMORY.INDEX.stdout++;
 		
 		if(str[i] == '\n') {
 			newline();
+		}
+		else if(str[i] == '\t') {
+			tab();
 		}
 		else if(str[i] == '&') {
 			if(str[i+1] == 'h') {
@@ -156,60 +134,60 @@ void print(const char str[]) {
 		} else { 
 			vram_char(str[i]);
 		}
-		vram_color(this_color);
+		vram_color(MEMORY.GLOBAL.color);
 	}
 	return;
 }
 
 void printf(color_t color, const char str[]) {
-	color_t old_color = this_color;
+	color_t old_color = MEMORY.GLOBAL.color;
 	setcolor(color);
 	print(str);
 	setcolor(old_color);
 }
 
 char* stdin(int8_t FLAG) {
-	VUPDATE = true;	//Flag for repaint initially
+	MEMORY.FLAGS.stdout = true;	//Flag for repaint initially
 	stdin_clear();
-	int16_t location = vptr;
+	int16_t location = MEMORY.INDEX.stdout;
 	int32_t counter = 0;
 	bool blink = true;
 	while(stdin_peek() != '\n') {
 		vmove(location);
 		
-		if(VUPDATE)	{ //Only update screen if a key was pressed
+		if(MEMORY.FLAGS.stdout)	{ //Only update screen if a key was pressed
 			switch(FLAG) {
-				case ECHO: 		print(std_stream); 						break;
-				case PASSWD: 	for(size_t i=0;i<stdin_size;i++)
+				case ECHO: 		print(MEMORY.STDIO.stdout); 						break;
+				case PASSWD: 	for(size_t i=0;i<MEMORY.INDEX.stdin;i++)
 					    			{ print("*"); } 							break;
 				case NOECHO:													break;
-				default: print(std_stream); 								break;
+				default: print(MEMORY.STDIO.stdout); 								break;
 			}
-			VUPDATE = false; //De-flag the update flag
+			MEMORY.FLAGS.stdout = false; //De-flag the update flag
 		}
-		vmove(location+stdin_size);
+		vmove(location+MEMORY.INDEX.stdin);
 		blink?print("_ "):print("  ");
 		if(counter > 65534*3) { counter = 0; blink = !blink; }
 		++counter;
 	}
 	stdin_pop(); //Remove trailing newline
-	vmove(location+stdin_size);
+	vmove(location+MEMORY.INDEX.stdin);
 	print(" "); //erase leftover blinky thing
-	return std_stream;
+	return MEMORY.STDIO.stdout;
 }
 
 void stdin_clear(void) {
-	VUPDATE = true;
-	std_stream[0] = '\0';
-	stdin_size=0;
+	MEMORY.FLAGS.stdout = true;
+	MEMORY.STDIO.stdout[0] = '\0';
+	MEMORY.INDEX.stdin=0;
 }
 
 void stdin_push(char c) {
-	VUPDATE = true;
-	if(stdin_size < STD_MAX) {
-		std_stream[stdin_size] = c;
-		std_stream[stdin_size+1] = '\0';
-		stdin_size++;
+	MEMORY.FLAGS.stdout = true;
+	if(MEMORY.INDEX.stdin < STD_MAX) {
+		MEMORY.STDIO.stdout[MEMORY.INDEX.stdin] = c;
+		MEMORY.STDIO.stdout[MEMORY.INDEX.stdin+1] = '\0';
+		MEMORY.INDEX.stdin++;
 	} 
 	else {
 		stdin_push(STD_MAX-1);
@@ -217,11 +195,11 @@ void stdin_push(char c) {
 }
 
 char stdin_pop(void) {
-	VUPDATE = true;
-	if(stdin_size > 0) {
-		stdin_size--;
-		std_stream[stdin_size] = '\0';
-		return std_stream[stdin_size-1];
+	MEMORY.FLAGS.stdout = true;
+	if(MEMORY.INDEX.stdin > 0) {
+		MEMORY.INDEX.stdin--;
+		MEMORY.STDIO.stdout[MEMORY.INDEX.stdin] = '\0';
+		return MEMORY.STDIO.stdout[MEMORY.INDEX.stdin-1];
 	} else {
 		stdin_clear();
 		return '\0';
@@ -229,8 +207,8 @@ char stdin_pop(void) {
 }
 
 char stdin_peek(void) {
-	if(stdin_size > 0) {
-		return std_stream[stdin_size-1];
+	if(MEMORY.INDEX.stdin > 0) {
+		return MEMORY.STDIO.stdout[MEMORY.INDEX.stdin-1];
 	} else {
 		return '\0';
 	}
