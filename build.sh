08cedeec3d7840d9fa679c807a885a623be114ec
BitOfAZeldaFan <bryan.geek64@gmail.com>
2016-01-29 18:10:37 -0600

    Flag warnings in MAKE

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo 'Assembling...' &&
 nasm -f elf src/boot.asm -o obj/boot.o &&
 
 echo 'Compiling...' &&
-gcc -m32 -c src/kernel.c -o obj/kernel.o -std=gnu99 -ffreestanding -O1 -W &&
+gcc -m32 -c src/kernel.c -o obj/kernel.o -std=gnu99 -ffreestanding -Wall -Wextra -O1 &&
 
 echo 'Linking...' &&
 ld -m elf_i386 -T linker.ld -o bin/kernel obj/boot.o obj/kernel.o &&
@@ -14,3 +14,4 @@ ld -m elf_i386 -T linker.ld -o bin/kernel obj/boot.o obj/kernel.o &&
 echo 'Running kernel emulator...' &&
 qemu-system-i386 -kernel bin/kernel -no-kvm &&
 echo 'Finished.'
+ 
