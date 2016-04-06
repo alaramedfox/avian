6bd794db0842ff9014ddbc9cef0f34806477ea52
bitofazeldafan <bryan.geek64@gmail.com>
2016-04-05 23:11:42 -0500

    Update to v0.8

--- src/include/buildcount.h
+++ src/include/buildcount.h
@@ -1,3 +1,3 @@
-#define BUILD "4139"
-#define TIMESTAMP "0.8.0.4139 - Tue Apr  5 23:07:41 CDT 2016"
-#define VERSION "0.8.0.4139"
+#define BUILD "4140"
+#define TIMESTAMP "0.8.0.4140 - Tue Apr  5 23:10:21 CDT 2016"
+#define VERSION "0.8.0.4140"
